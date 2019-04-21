40c909fb582b6896ee941cc89b29d6671ab1838d
Kat B <katreesebarnes@gmail.com>
2019-04-21 19:14:58 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -4,6 +4,8 @@
   MusicImporter.new(path).import
   end
   def call 
+    input = "" 
+    while input != exit
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
