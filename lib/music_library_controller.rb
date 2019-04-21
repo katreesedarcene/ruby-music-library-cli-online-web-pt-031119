20518e41d65846717d101f6f41dcf402acb6e846
Kat B <katreesebarnes@gmail.com>
2019-04-21 18:37:41 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,5 +7,6 @@
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in yourlibrary, enter 'list genres'."
   end
  end
