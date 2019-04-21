3b3741332060670492653f19994b243af3eba318
Kat B <katreesebarnes@gmail.com>
2019-04-21 19:39:50 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -45,5 +45,10 @@
   def list_songs_by_genre
     puts "Please enter the name of a genre:"
     input = gets.strip
+     if artist = Genre.find_by_name(input)
+      artist.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+        puts "#{i}. #{s.name} - #{s.genre.name}"
+      end
+    end
   end
  end
